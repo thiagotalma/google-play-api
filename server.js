6a559e056a535abfd3a358d183fcb77e6dd8b207
Thiago Talma <em.gitkraken@mailnull.com>
2019-03-07 12:09:58 -0300

    heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const router = require('./lib');
 const app = Express();
 const port = process.env.PORT || 3000;
 
-app.use('/api/', router);
+app.use('/', router);
 
 app.listen(port, function () {
   console.log('Server started on port', port);
